7d626401cea6c6ea3d2195e991ca280d421c02c8
Reinier <sasuke.reinier@gmail.com>
2021-04-19 13:57:53 -0400

    actualizando codigo js para que use logo.svg en vez de logo.png

--- js/menu-color.js
+++ js/menu-color.js
@@ -6,7 +6,7 @@
        $('.navbar-brand img').attr('src', 'img/logo-light.png');
     } else {
        $('.icon-bar').removeClass('opaque');
-       $('.navbar-brand img').attr('src', 'img/logo.png');
+       $('.navbar-brand img').attr('src', 'img/logo.svg');
     }
 });
 
@@ -17,12 +17,12 @@ $(function(){
         if($(this).scrollTop() > 100) {
             $('#topbar, .cart-label').fadeOut('slow');
             $('.logo img')
-                .attr('src','img/logo.png');
+                .attr('src','img/logo.svg');
         }
         if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
             $('.logo img')
-                .attr('src','img/logo-light.png');
+                .attr('src','img/logo-light.svg');
         }
     });
 });
